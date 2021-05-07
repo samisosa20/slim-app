334cde54b880fe8f029be76e7c2b10dfacb4eef0
Sammy Guttman <1449@holbertonschool.com>
2021-05-07 07:30:56 -0500

    first commit

---
+++ app/routes.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+use App\Application\Actions\User\ListUsersAction;
+use App\Application\Actions\User\ViewUserAction;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
+return function (App $app) {
+    $app->options('/{routes:.*}', function (Request $request, Response $response) {
+        // CORS Pre-Flight OPTIONS Request Handler
+        return $response;
+    });
+
+    $app->group('/api/v1', function (Group $group) {
+        $group->get('', function (Request $request, Response $response) {
+            $response->getBody()->write('Hello world!');
+            return $response;
+        });
+        $group->get('/clients', 'App\Application\Controllers\ClientsController\ClientsController:getAll');
+    });
+
+    $app->group('/users', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+        $group->get('/{id}', ViewUserAction::class);
+    });
+};
---
+++ src/config/db.php
@@ -0,0 +1,15 @@
+<?php
+    class db{
+        private $dbHost ='localhost';
+        private $dbUser ='root';
+        private $dbPass ='Gpax@FSn';
+        private $dbName ='test';
+
+        //connection
+        public function connectionDB(){
+            $mysqlConnect = "mysql:host=$this->dbHost;dbname=$this->dbName";
+            $dbConnection = new PDO($mysqlConnect, $this->dbUser, $this->dbPass);
+            $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $dbConnection;
+        }
+    }
